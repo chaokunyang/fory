97a318acd8bc292ceb78dd981e073587ea204a68
chaokunyang <shawn.ck.yang@gmail.com>
2024-05-03 13:06:03 +0800

    remove fury data provider

--- java/fury-core/src/test/java/org/apache/fury/CyclicTest.java
+++ java/fury-core/src/test/java/org/apache/fury/CyclicTest.java
@@ -65,7 +65,7 @@ public class CyclicTest extends FuryTestBase {
         .toArray(Object[][]::new);
   }
 
-  @Test(dataProvider = "fury")
+  @Test
   public void testBean(FuryBuilder builder) {
     Fury fury = builder.withMetaContextShare(false).withRefTracking(true).build();
     for (Object[] objects : beans()) {
@@ -87,7 +87,7 @@ public class CyclicTest extends FuryTestBase {
     }
   }
 
-  @Test(dataProvider = "fury")
+  @Test
   public void testBeanMetaShared(FuryBuilder builder) {
     Fury fury = builder.withMetaContextShare(true).withRefTracking(true).build();
     for (Object[] objects : beans()) {

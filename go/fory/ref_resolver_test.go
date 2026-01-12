7f17598253ee90cdff42699057df88b005d2756e
chaokunyang <shawn.ck.yang@gmail.com>
2026-01-12 11:38:33 +0800

    fix test

--- go/fory/ref_resolver_test.go
+++ go/fory/ref_resolver_test.go
@@ -148,7 +148,7 @@ func TestRefTrackingLargeCount(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			f := New(WithRefTracking(true))
+			f := New(WithXlang(true), WithRefTracking(true))
 
 			err := f.RegisterNamedStruct(&Inner{}, fmt.Sprintf("RefTest_Inner_%d", tt.count))
 			require.NoError(t, err)

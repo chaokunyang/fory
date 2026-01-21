3ed52c9c75b02c6923843380931c2800d3a6149f
chaokunyang <shawn.ck.yang@gmail.com>
2026-01-22 00:05:44 +0800

    add java unsigned array support

--- java/fory-core/src/main/java/org/apache/fory/type/Descriptor.java
+++ java/fory-core/src/main/java/org/apache/fory/type/Descriptor.java
@@ -50,9 +50,13 @@ import org.apache.fory.annotation.Ignore;
 import org.apache.fory.annotation.Int32Type;
 import org.apache.fory.annotation.Int64Type;
 import org.apache.fory.annotation.Internal;
+import org.apache.fory.annotation.Uint16ArrayType;
 import org.apache.fory.annotation.Uint16Type;
+import org.apache.fory.annotation.Uint32ArrayType;
 import org.apache.fory.annotation.Uint32Type;
+import org.apache.fory.annotation.Uint64ArrayType;
 import org.apache.fory.annotation.Uint64Type;
+import org.apache.fory.annotation.Uint8ArrayType;
 import org.apache.fory.annotation.Uint8Type;
 import org.apache.fory.collection.Collections;
 import org.apache.fory.collection.Tuple2;
@@ -683,6 +687,10 @@ public class Descriptor {
     typeAnnotationsTypes.add(Uint16Type.class);
     typeAnnotationsTypes.add(Uint32Type.class);
     typeAnnotationsTypes.add(Uint64Type.class);
+    typeAnnotationsTypes.add(Uint8ArrayType.class);
+    typeAnnotationsTypes.add(Uint16ArrayType.class);
+    typeAnnotationsTypes.add(Uint32ArrayType.class);
+    typeAnnotationsTypes.add(Uint64ArrayType.class);
   }
 
   public static Annotation getAnnotation(Field field) {

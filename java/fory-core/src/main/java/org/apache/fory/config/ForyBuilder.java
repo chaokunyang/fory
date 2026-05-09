c0a5a8db1378e919cc9c5710e618e9eb0a443617
慕白 <chaokun.yck@antgroup.com>
2026-05-10 00:20:53 +0800

    feat(java): finalize Android codegen defaults

--- java/fory-core/src/main/java/org/apache/fory/config/ForyBuilder.java
+++ java/fory-core/src/main/java/org/apache/fory/config/ForyBuilder.java
@@ -31,6 +31,7 @@ import org.apache.fory.logging.Logger;
 import org.apache.fory.logging.LoggerFactory;
 import org.apache.fory.meta.DeflaterMetaCompressor;
 import org.apache.fory.meta.MetaCompressor;
+import org.apache.fory.platform.AndroidSupport;
 import org.apache.fory.platform.GraalvmSupport;
 import org.apache.fory.pool.ThreadPoolFory;
 import org.apache.fory.reflect.ReflectionUtils;
@@ -85,7 +86,7 @@ public final class ForyBuilder {
   boolean requireClassRegistration = true;
   Boolean metaShareEnabled;
   Boolean scopedMetaShareEnabled;
-  boolean codeGenEnabled = true;
+  Boolean codeGenEnabled;
   Boolean deserializeUnknownClass;
   boolean asyncCompilationEnabled = false;
   boolean registerGuavaTypes = true;
@@ -441,14 +442,14 @@ public final class ForyBuilder {
   /**
    * Whether enable jit for serialization. When disabled, the first serialization will be faster
    * since no need to generate code, but later will be much slower compared jit mode.
+   *
+   * <p>When unset, codegen defaults to disabled on Android and GraalVM native image, and enabled on
+   * ordinary JVM. Explicitly enabling codegen on a platform without runtime code generation support
+   * is accepted by the builder, but final build configuration will force interpreter serializers.
    */
   public ForyBuilder withCodegen(boolean codeGen) {
-    if (GraalvmSupport.IN_GRAALVM_NATIVE_IMAGE) {
-      codeGen = true;
-    }
     this.codeGenEnabled = codeGen;
-    boolean enableCodegen = codeGen;
-    recordAction(b -> b.withCodegen(enableCodegen));
+    recordAction(b -> b.withCodegen(codeGen));
     return this;
   }
 
@@ -628,8 +629,27 @@ public final class ForyBuilder {
                 + "`ForyBuilder#withTypeChecker` or `TypeResolver#setTypeChecker`");
       }
     }
-    if (GraalvmSupport.IN_GRAALVM_NATIVE_IMAGE && asyncCompilationEnabled) {
-      LOG.info("Use sync compilation for graalvm native image since it doesn't support JIT.");
+    finishCodegen();
+  }
+
+  private void finishCodegen() {
+    boolean runtimeCodegenUnsupported =
+        AndroidSupport.IS_ANDROID || GraalvmSupport.IN_GRAALVM_NATIVE_IMAGE;
+    boolean resolvedCodegen =
+        codeGenEnabled != null ? codeGenEnabled.booleanValue() : !runtimeCodegenUnsupported;
+    if (runtimeCodegenUnsupported && resolvedCodegen) {
+      if (Boolean.TRUE.equals(codeGenEnabled)) {
+        LOG.warn(
+            "The current platform does not support Fory runtime code generation; "
+                + "interpreter serializers will be used instead.");
+      }
+      resolvedCodegen = false;
+    }
+    codeGenEnabled = resolvedCodegen;
+    if (runtimeCodegenUnsupported && asyncCompilationEnabled) {
+      LOG.info(
+          "Async compilation is disabled because the current platform does not support Fory "
+              + "runtime code generation.");
       asyncCompilationEnabled = false;
     }
   }

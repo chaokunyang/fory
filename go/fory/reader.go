e681fccfae78096e0974c32291f591a5b6564609
chaokunyang <shawn.ck.yang@gmail.com>
2026-08-07 12:03:44 +0800

    perf(go): avoid copying error state

--- go/fory/reader.go
+++ go/fory/reader.go
@@ -171,7 +171,7 @@ func (c *ReadContext) RefResolver() *RefResolver {
 
 // HasError returns true if an error has occurred
 func (c *ReadContext) HasError() bool {
-	return c.err.kind != ErrKindOK
+	return c.err.HasError()
 }
 
 // Err returns a pointer to the accumulated error for passing to buffer methods
